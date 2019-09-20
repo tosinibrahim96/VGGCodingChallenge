ce49d88fd2c4ae4c1f8ee89a80f8d59d208d8689
ibrahimalausa <ibrahim.alausa@andela.com>
2019-09-20 14:53:22 +0100

    final update

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ function processData(input) {
     console.log(1);
   }
 }
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+_input = '';
+process.stdin.on('data', function(input) {
+  _input += input;
+});
+
+process.stdin.on('end', function() {
+  processData(_input);
+});
